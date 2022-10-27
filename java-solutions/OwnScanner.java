59245e315e124bab642dd0d72ca1630315ec7daf
Michael Ruzavin <misharuz25@gmail.com>
2022-10-27 16:15:46 +0300

    Checked by an instructor

--- java-solutions/OwnScanner.java
+++ java-solutions/OwnScanner.java
@@ -1,5 +1,37 @@
 import java.io.*;
 
+// Testing Base
+//    Running test 01: java Reverse [1 input lines]
+//Exception in thread "main" java.lang.AssertionError: Line 1:
+//     expected `1`,
+//       actual ``
+//        at base.Asserts.error(Asserts.java:75)
+//        at base.Asserts.assertTrue(Asserts.java:41)
+//        at base.Asserts.assertEquals(Asserts.java:20)
+//        at base.Runner.lambda$testEquals$0(Runner.java:36)
+//        at base.TestCounter.lambda$test$0(TestCounter.java:58)
+//        at base.TestCounter.lambda$testV$2(TestCounter.java:71)
+//        at base.Log.silentScope(Log.java:40)
+//        at base.TestCounter.testV(TestCounter.java:70)
+//        at base.TestCounter.test(TestCounter.java:57)
+//        at base.Runner.testEquals(Runner.java:30)
+//        at reverse.ReverseTester$Checker.test(ReverseTester.java:102)
+//        at reverse.ReverseTester$Checker.test(ReverseTester.java:96)
+//        at reverse.ReverseTester$Checker.test(ReverseTester.java:137)
+//        at reverse.ReverseTester.run(ReverseTester.java:67)
+//        at reverse.ReverseTester.lambda$variant$2(ReverseTester.java:44)
+//        at base.Selector.lambda$test$2(Selector.java:79)
+//        at base.Log.lambda$action$0(Log.java:18)
+//        at base.Log.silentScope(Log.java:40)
+//        at base.Log.scope(Log.java:31)
+//        at base.Log.scope(Log.java:24)
+//        at base.Selector.lambda$test$3(Selector.java:79)
+//        at java.base/java.util.ArrayList.forEach(ArrayList.java:1511)
+//        at base.Selector.test(Selector.java:79)
+//        at base.Selector.main(Selector.java:51)
+//        at reverse.FullFastReverseTest.main(FullFastReverseTest.java:15)
+//ERROR: Tests: failed
+
 public class OwnScanner {
 
     private final Reader reader;
