5e44ab223a8155677b1136bf7da1fb3dd01653cb
GrigoriyKarpuhin <grischa.karpukhin@yandex.ru>
2022-10-27 14:53:31 +0300

    Homework 5 added

--- java-solutions/OwnScanner.java
+++ java-solutions/OwnScanner.java
@@ -8,59 +8,70 @@ public class OwnScanner {
     private String wordcheck;
     private boolean switchInt;
     private boolean switchWord;
-    private boolean switchIntSpace;
+    private boolean switchSpace;
     private boolean hasInt;
     private boolean hasWord;
     private boolean hasLetters;
-    private final char[] buffer = new char[1024]; // read = reader.read(buffer, 0, buffer.length);
+    private final char[] buffer = new char[512];
     private int bufferSize;
+    private int number = 0;
+    private boolean hasNumber;
+    private String numbercheck;
+    private boolean switchNumber;
 
-    public OwnScanner(String line) {
+    public OwnScanner(String line)  {
         this.reader = new StringReader(line);
+        BufferCreate();
     }
 
     public OwnScanner(InputStream stream) {
         this.reader = new InputStreamReader((stream));
+        BufferCreate();
     }
 
-    public OwnScanner(String file, String code) throws FileNotFoundException, UnsupportedEncodingException {
+    public OwnScanner(String file, String code) {
+        try {
             this.reader = new InputStreamReader(new FileInputStream(file), code);
-    }
-
-    public char[] BufferCreate() throws IOException {
-        bufferSize = reader.read(buffer);
-        return buffer;
-    }
-    public String nextLine() throws IOException {
-        if (stringcheck.length() != 0) {
-            return stringcheck;
-        } else {
-            StringBuilder sbStr = new StringBuilder();
-            int read = reader.read();
-            while (read >= 0) {
-                if (read == '\n') {
-                    break;
-                } else {
-                    sbStr.append((char) read);
-                    read = reader.read();
-                }
-            }
-            return sbStr.toString();
+        } catch (UnsupportedEncodingException | FileNotFoundException e) {
+            throw new RuntimeException(e);
         }
+        BufferCreate();
     }
 
-    public boolean hasNextLine() throws IOException {
-        int read = reader.read();
-        if (read < 0) {
+    public void BufferCreate() {
+        try {
+            bufferSize = reader.read(buffer);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        number = 0;
+    }
+    public String nextLine() {
+        return stringcheck;
+    }
+
+    public boolean hasNextLine() {
+        if (bufferSize <= 0) {
             return false;
         } else {
-            StringBuilder sbStr = new StringBuilder();
-            while (read >= 0) {
-                if (read == '\n') {
+			int read = buffer[number];
+			number++;
+			if (number == bufferSize) {
+                BufferCreate();
+            }
+			StringBuilder sbStr = new StringBuilder();
+            while (number < bufferSize) {
+                if (System.lineSeparator().length() == 1 && (read == '\r' || read == '\n')) {
                     break;
-                } else {
-                    sbStr.append((char) read);
-                    read = reader.read();
+                }
+                if (System.lineSeparator().length() == 2 && read == '\n') {
+                    break;
+                }
+                sbStr.append((char) read);
+                read = buffer[number];
+                number++;
+                if (number == bufferSize) {
+                    BufferCreate();
                 }
             }
             stringcheck = sbStr.toString();
@@ -68,32 +79,40 @@ public class OwnScanner {
         }
     }
 
-    public int nextInt() throws IOException {
+    public int nextInt() {
         if (hasInt && !switchInt) {
             switchInt = true;
             return intcheck;
         } else {
             StringBuilder sbInt = new StringBuilder();
-            int read = reader.read();
-            while (read >= 0) {
+            int read = buffer[number];
+            number++;
+            if (number == bufferSize) {
+                BufferCreate();
+            }
+            while (number < bufferSize) {
                 if (Character.isDigit((char) read) || (char)read == '-') {
-                    switchIntSpace = true;
+                    switchSpace = true;
                     sbInt.append((char) read);
                 } else {
-                    if (switchIntSpace) {
-                        switchIntSpace = false;
+                    if (switchSpace) {
+                        switchSpace = false;
                         switchInt = false;
                         return Integer.parseInt(sbInt.toString());
                     }
                 }
-                read = reader.read();
+                read = buffer[number];
+                number++;
+                if (number == bufferSize) {
+                    BufferCreate();
+                }
             }
             switchInt = false;
             return Integer.parseInt(sbInt.toString());
         }
     }
 
-    public boolean hasNextInt() throws IOException {
+    public boolean hasNextInt() {
         try {
             intcheck = nextInt();
             return hasInt = true;
@@ -102,15 +121,19 @@ public class OwnScanner {
         }
     }
 
-    public String nextWord() throws IOException{
+    public String nextWord() {
         if (hasWord && !switchWord) {
             switchWord = true;
             return wordcheck;
         } else {
             StringBuilder letters = new StringBuilder();
             String word;
-            int read = reader.read();
-            while (read >= 0) {
+            int read = buffer[number];
+            number++;
+			if (number == bufferSize) {
+                BufferCreate();
+            }
+            while (number < bufferSize) {
                 if (Character.isLetter(read) || read == '\'' || Character.DASH_PUNCTUATION == Character.getType(read)) {
                     hasLetters = true;
                     letters.append((char) read);
@@ -123,14 +146,18 @@ public class OwnScanner {
                         return word;
                     }
                 }
-                read = reader.read();
+                read = buffer[number];
+                number++;
+                if (number == bufferSize) {
+                    BufferCreate();
+                }
             }
             switchWord = false;
             return letters.toString();
         }
     }
-    
-    public boolean hasNextWord() throws IOException {
+
+    public boolean hasNextWord()  {
         wordcheck = nextWord();
         if (wordcheck.length() != 0) {
             return hasWord = true;
@@ -138,9 +165,53 @@ public class OwnScanner {
             return hasWord = false;
         }
     }
+    public String nextNumber() {
+        if (hasNumber && !switchNumber) {
+            switchNumber = true;
+            return numbercheck;
+        } else {
+            StringBuilder sb = new StringBuilder();
+            int read = buffer[number];
+            number++;
+            if (number == bufferSize) {
+                BufferCreate();
+            }
+            while (number < bufferSize) {
+                if (Character.isDigit((char) read) || (char)read == '-') {
+                    switchSpace = true;
+                    sb.append((char) read);
+                } else {
+                    if (switchSpace) {
+                        switchSpace = false;
+                        switchNumber = false;
+                        return sb.toString();
+                    }
+                }
+                read = buffer[number];
+                number++;
+                if (number == bufferSize) {
+                    BufferCreate();
+                }
+            }
+            switchNumber = false;
+            return String.valueOf(sb);
+        }
+    }
 
-    public void close() throws IOException {
-        reader.close();
+    public boolean hasNextNumber() {
+            numbercheck = nextNumber();
+            if (numbercheck.length() > 0) {
+                return hasNumber = true;
+            } else {
+            return hasNumber = false;
+        }
+    }
+    public void close() {
+        try {
+            reader.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 
--- java-solutions/ReverseOct.java
+++ java-solutions/ReverseOct.java
@@ -1,29 +1,31 @@
-import java.io.IOException;
+
 import java.util.Arrays;
 //
-public class Reverse {
+public class ReverseOct {
     public static void main(String[] args) {
-	    int[][] arrInts = new int[1][1];
+	    String[][] arrInts = new String[1][1];
 		int column = 0;
 		OwnScanner scan1 = new OwnScanner(System.in);
 		while (scan1.hasNextLine()) {
 			if (column == arrInts.length) {
 			    arrInts = Arrays.copyOf(arrInts, arrInts.length * 2);
 			}	
-			arrInts[column] = new int[1];
+			arrInts[column] = new String[1];
 			int line = 0;
 			String lines = scan1.nextLine();
 			OwnScanner scan2 = new OwnScanner(lines);
-            while (scan2.hasNextInt()) {			
+            while (scan2.hasNextNumber()) {
 				if (line == arrInts[column].length) {
 					arrInts[column] = Arrays.copyOf(arrInts[column], arrInts[column].length * 2);
 			    } 
-				arrInts[column][line] = scan2.nextInt();
+				arrInts[column][line] = scan2.nextNumber();
 				line += 1;		
 		    }
+			scan2.close();
 			arrInts[column] = Arrays.copyOf(arrInts[column], line);
 			column += 1;
 		}
+		scan1.close();
 		arrInts = Arrays.copyOf(arrInts, column);
         for (int i = column - 1; i >= 0; i--) {
 	        for (int j = arrInts[i].length - 1; j >= 0; j--) {
