ed9cd887bd45d3839013790995673a7fc78ae413
GrigoriyKarpuhin <grischa.karpukhin@yandex.ru>
2022-12-07 17:47:03 +0300

    fix

--- java-solutions/WordStatInput.java
+++ java-solutions/WordStatInput.java
@@ -1,39 +1,48 @@
 import java.io.*;
 import java.util.*;
-
-
 //
 public class WordStatInput {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		LinkedHashMap<String, Integer> words = new LinkedHashMap<>();
-		OwnScanner scan1 = new OwnScanner(args[0], "utf8");
-		while (scan1.hasNextLine()) {
-			String line = scan1.nextLine();
-			OwnScanner scan2 = new OwnScanner(line);
-			while (scan2.hasNextWord()) {
-				String lettersSt = scan2.nextWord();
-				if (words.containsKey(lettersSt.toLowerCase())) {
-					words.replace(lettersSt.toLowerCase(), words.get(lettersSt.toLowerCase()) + 1);
-				} else {
-					words.put(lettersSt.toLowerCase(), 1);
-				}
-			}
-			scan2.close();
-		}
-		scan1.close();
+		StringBuilder letters = new StringBuilder();
+		Reader reader = new BufferedReader(new InputStreamReader(
+			new FileInputStream(args[0]),
+			"utf8"
+		));
 		try {
-			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
-					new FileOutputStream(args[1]),
-					"utf8"
-			));
-			Set<String> key = words.keySet();
-				for (String s : key) {
-					writer.write(s + " " + words.get(s));
-					writer.newLine();
+			int read = reader.read();
+			while (read >= 0) {
+				if (Character.isLetter(read) == true || read == '\'' || Character.DASH_PUNCTUATION == Character.getType(read)) {
+					   letters.append((char)read);	
+				} else {
+					String lettersSt = letters.toString();
+					if (words.containsKey(lettersSt.toLowerCase())) {
+					words.replace(lettersSt.toLowerCase(), words.get(lettersSt.toLowerCase()) + 1);
+					letters.setLength(0);	
+					} else {
+						if (letters.length() != 0) {
+							words.put(lettersSt.toLowerCase(), 1);
+							letters.setLength(0);
+						}
+					}
 				}
-				writer.close();
-		} catch (IOException e) {
-			throw new RuntimeException(e);
+				read = reader.read();
+			}
+		} finally {
+			reader.close();
+		}
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
+			new FileOutputStream(args[1]),
+			"utf8"
+		));
+		Set<String> key = words.keySet();
+		try {
+			for(String s: key) {
+				writer.write(s + " " + words.get(s));
+				writer.newLine();
+			}
+		} finally {
+			writer.close();
 		}
 	}
 }
